6283066614da04c60d942a673bacb621265a2db0
bfredl <bjorn.linse@gmail.com>
2023-06-01 21:52:45 +0200

    struktur

--- src/itrace.zig
+++ src/itrace.zig
@@ -5,10 +5,12 @@ const info = std.log.info;
 const util = @import("./util.zig");
 const ioctlnr = util.ioctlnr;
 const mem = std.mem;
+const print = std.debug.print;
+const fd_t = linux.fd_t;
 
 const c = @import("./c.zig");
 
-pub fn retry_ioctl(fd: linux.fd_t, request: u32, arg: usize) usize {
+pub fn retry_ioctl(fd: fd_t, request: u32, arg: usize) usize {
     while (true) {
         const status = linux.ioctl(fd, request, arg);
         const err = linux.getErrno(status);
@@ -16,59 +18,73 @@ pub fn retry_ioctl(fd: linux.fd_t, request: u32, arg: usize) usize {
     }
 }
 
-pub fn intel_ioctl(fd: linux.fd_t, dir: util.Dir, num: u32, arg: anytype) usize {
+pub fn intel_ioctl(fd: fd_t, dir: util.Dir, num: u32, arg: anytype) !usize {
     const typ = @typeInfo(@TypeOf(arg)).Pointer.child;
-    const cmd = ioctlnr(dir, num, @sizeOf(typ));
-    info("begå {s}: {}", .{ @typeName(typ), cmd });
-    const status = retry_ioctl(fd, cmd, @ptrToInt(arg));
+    const cmdnr = ioctlnr(dir, num, @sizeOf(typ));
+    info("begå {s}: {}", .{ @typeName(typ), cmdnr });
+    const status = retry_ioctl(fd, cmdnr, @ptrToInt(arg));
     const err = linux.getErrno(status);
     if (err != .SUCCESS) {
         // FIXME: What do we do if this fails?
         info("the cow jumped over the moon: {}", .{err});
+        return error.IntelError;
     }
     return status;
 }
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
     const fd = (try fs.cwd().openFileZ("/dev/dri/renderD128", .{ .mode = .read_write })).handle;
     info("the fd {}\n", .{fd});
 
     var create = mem.zeroes(c.drm_i915_gem_context_create);
-    const cret = intel_ioctl(fd, .WR, c.DRM_I915_GEM_CONTEXT_CREATE, &create);
-    if (cret != 0) {
-        return;
-    }
+    _ = try intel_ioctl(fd, .WR, c.DRM_I915_GEM_CONTEXT_CREATE, &create);
 
     const context_id = create.ctx_id;
     info("fin kontext: {}", .{context_id});
 
     const size = 4 * 4096;
 
-    var gem_create = mem.zeroInit(c.drm_i915_gem_create, .{
+    const gem_handle = try gem_create(fd, size);
+
+    const ptr = try gem_mmap(fd, size, gem_handle);
+    info("pekaren: {}", .{ptr});
+
+    var batch = CmdBatch.init(allocator);
+    try batch.append(cmd.pipeline_select(2));
+    for (batch.items) |i| {
+        print("0x{x:08}\n", .{i});
+    }
+    defer batch.deinit();
+}
+
+fn gem_create(fd: fd_t, size: usize) !u32 {
+    var drm_gem_create = mem.zeroInit(c.drm_i915_gem_create, .{
         .size = size,
     });
 
-    var ret = intel_ioctl(fd, .WR, c.DRM_I915_GEM_CREATE, &gem_create);
-    if (ret != 0) {
-        return;
-    }
+    _ = try intel_ioctl(fd, .WR, c.DRM_I915_GEM_CREATE, &drm_gem_create);
 
-    info("fin handel: {}", .{gem_create.handle});
-    const gem_handle = gem_create.handle;
+    return drm_gem_create.handle;
+}
 
-    var gem_mmap = mem.zeroInit(c.drm_i915_gem_mmap_offset, .{
-        .handle = gem_handle,
+fn gem_mmap(fd: fd_t, size: usize, handle: u32) !usize {
+    var gem_mmap_param = mem.zeroInit(c.drm_i915_gem_mmap_offset, .{
+        .handle = handle,
         .flags = c.I915_MMAP_OFFSET_WB,
     });
-    info("fina: {}", .{c.DRM_I915_GEM_MMAP_GTT});
-    ret = intel_ioctl(fd, .WR, c.DRM_I915_GEM_MMAP_GTT, &gem_mmap);
-    if (ret != 0) {
-        return;
-    }
+    _ = try intel_ioctl(fd, .WR, c.DRM_I915_GEM_MMAP_GTT, &gem_mmap_param);
 
-    info("ofsetten: {}", .{gem_mmap});
-
-    const ptr = linux.mmap(null, size, linux.PROT.WRITE | linux.PROT.READ, linux.MAP.SHARED, fd, @intCast(i64, gem_mmap.offset));
-
-    info("pekaren: {}", .{ptr});
+    return linux.mmap(null, size, linux.PROT.WRITE | linux.PROT.READ, linux.MAP.SHARED, fd, @intCast(i64, gem_mmap_param.offset));
 }
+
+const CmdBatch = std.ArrayList(u32);
+const cmd = struct {
+    pub fn pipeline_select(pipeline: u2) u32 {
+        const base: u32 = ((0x3 << 29) | (0x1 << 27) | (0x1 << 24) | (0x4 << 16) | (0x3 << 8));
+        return base + pipeline;
+    }
+};
