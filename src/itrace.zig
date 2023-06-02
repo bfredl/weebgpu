029d85d2374fa0a85fc67740e050785e93633917
bfredl <bjorn.linse@gmail.com>
2023-06-02 21:12:29 +0200

    mjuuuuuuuUUUUUUUU

--- src/itrace.zig
+++ src/itrace.zig
@@ -10,6 +10,72 @@ const fd_t = linux.fd_t;
 
 const c = @import("./c.zig");
 
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const fd = (try fs.cwd().openFileZ("/dev/dri/renderD128", .{ .mode = .read_write })).handle;
+    info("the fd {}\n", .{fd});
+
+    var create = mem.zeroes(c.drm_i915_gem_context_create);
+    _ = try intel_ioctl(fd, .WR, c.DRM_I915_GEM_CONTEXT_CREATE, &create);
+
+    const context_id = create.ctx_id;
+    info("fin kontext: {}", .{context_id});
+
+    const size = 4 * 4096;
+
+    const gem_handle = try gem_create(fd, size);
+
+    const obj = try gem_mmap(fd, size, gem_handle);
+    info("objektet: {}", .{obj});
+
+    var batch = CmdBatch.init(allocator);
+    try batch.append(cmd.pipeline_select(2));
+    for (batch.items) |i| {
+        print("0x{x:08}\n", .{i});
+    }
+    defer batch.deinit();
+
+    try execute_batch_simple(fd, context_id, obj, batch.items);
+}
+
+// ptr must be a mapping of gem_handle
+pub fn execute_batch_simple(fd: fd_t, context_id: u32, obj: GemObj, batch: []u32) !void {
+    // TODO assert size!
+    @memcpy(@intToPtr([*]u32, obj.ptr)[0..batch.len], batch);
+    // TODO: clflush each cacheline?
+    @fence(.Release);
+
+    var object: util.drm_i915_gem_exec_object2 = .{
+        .handle = obj.handle,
+        .relocation_count = 0,
+        .relocs_ptr = 0,
+        .alignment = 0,
+        .offset = 0xFFFFFFFFFFFFFFFF,
+        .flags = 0,
+        .rsvd1 = 0,
+        .rsvd2 = 0,
+    };
+
+    var execbuf: c.drm_i915_gem_execbuffer2 = .{
+        .buffers_ptr = @ptrToInt(&object),
+        .buffer_count = 1,
+        .batch_start_offset = 0,
+        .batch_len = @intCast(c_uint, 4 * batch.len),
+        .flags = c.I915_EXEC_HANDLE_LUT | c.I915_EXEC_NO_RELOC, // TODO: mere
+        .rsvd1 = context_id,
+        .rsvd2 = 0,
+        .DR1 = 0,
+        .DR4 = 0,
+        .num_cliprects = 0,
+        .cliprects_ptr = 0,
+    };
+
+    _ = try intel_ioctl(fd, .WR, c.DRM_I915_GEM_EXECBUFFER2, &execbuf);
+}
+
 pub fn retry_ioctl(fd: fd_t, request: u32, arg: usize) usize {
     while (true) {
         const status = linux.ioctl(fd, request, arg);
@@ -32,35 +98,6 @@ pub fn intel_ioctl(fd: fd_t, dir: util.Dir, num: u32, arg: anytype) !usize {
     return status;
 }
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer _ = gpa.deinit();
-
-    const fd = (try fs.cwd().openFileZ("/dev/dri/renderD128", .{ .mode = .read_write })).handle;
-    info("the fd {}\n", .{fd});
-
-    var create = mem.zeroes(c.drm_i915_gem_context_create);
-    _ = try intel_ioctl(fd, .WR, c.DRM_I915_GEM_CONTEXT_CREATE, &create);
-
-    const context_id = create.ctx_id;
-    info("fin kontext: {}", .{context_id});
-
-    const size = 4 * 4096;
-
-    const gem_handle = try gem_create(fd, size);
-
-    const ptr = try gem_mmap(fd, size, gem_handle);
-    info("pekaren: {}", .{ptr});
-
-    var batch = CmdBatch.init(allocator);
-    try batch.append(cmd.pipeline_select(2));
-    for (batch.items) |i| {
-        print("0x{x:08}\n", .{i});
-    }
-    defer batch.deinit();
-}
-
 fn gem_create(fd: fd_t, size: usize) !u32 {
     var drm_gem_create = mem.zeroInit(c.drm_i915_gem_create, .{
         .size = size,
@@ -71,14 +108,27 @@ fn gem_create(fd: fd_t, size: usize) !u32 {
     return drm_gem_create.handle;
 }
 
-fn gem_mmap(fd: fd_t, size: usize, handle: u32) !usize {
+const GemObj = struct {
+    handle: u32,
+    size: usize,
+    offset: usize,
+    ptr: usize,
+};
+
+fn gem_mmap(fd: fd_t, size: usize, handle: u32) !GemObj {
     var gem_mmap_param = mem.zeroInit(c.drm_i915_gem_mmap_offset, .{
         .handle = handle,
         .flags = c.I915_MMAP_OFFSET_WB,
     });
     _ = try intel_ioctl(fd, .WR, c.DRM_I915_GEM_MMAP_GTT, &gem_mmap_param);
 
-    return linux.mmap(null, size, linux.PROT.WRITE | linux.PROT.READ, linux.MAP.SHARED, fd, @intCast(i64, gem_mmap_param.offset));
+    const ptr = linux.mmap(null, size, linux.PROT.WRITE | linux.PROT.READ, linux.MAP.SHARED, fd, @intCast(i64, gem_mmap_param.offset));
+    return .{
+        .handle = handle,
+        .size = size,
+        .offset = gem_mmap_param.offset,
+        .ptr = ptr,
+    };
 }
 
 const CmdBatch = std.ArrayList(u32);
